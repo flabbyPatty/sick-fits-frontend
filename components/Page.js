d36dd0294c1ff6f55eb785875bef2aa96fac34e9
Patrick Santos <pgsantos.92@gmail.com>
2022-07-27 20:48:15 +0800

    Create page layout component

---
+++ components/Page.js
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types';
+
+export default function Page({ children, cool }) {
+  return (
+    <div>
+      <h2>I am the page component</h2>
+      {children}
+      {cool}
+    </div>
+  );
+}
+
+Page.propTypes = {
+  cool: PropTypes.string,
+  children: PropTypes.any,
+};
